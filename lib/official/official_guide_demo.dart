896e0dcf5751d287f0b8ef239081341567dfa03b
qonyqian <qonyqian@tencent.com>
2021-12-03 16:28:24 +0800

    妹子长廊添加数据库操作

--- lib/official/official_guide_demo.dart
+++ lib/official/official_guide_demo.dart
@@ -283,4 +283,5 @@ class BeautifulGirlDetailPage extends StatelessWidget {
 
 //todo
 //6.继续创建我的相册demo，加入喜欢功能和跳转详情功能，图片tab滑动功能。
-//7.图片长廊添加数据库操作
+//7.整理我的demo
+//8.https://momoyu.cc/   用这个api写一个app
--- lib/official/official_guide_girl_gallery.dart
+++ lib/official/official_guide_girl_gallery.dart
@@ -3,6 +3,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:my_first_flutter/official/official_guide_model.dart';
 
 import 'custom_my_widget.dart';
+import 'official_guide_utils.dart';
 
 /// 图片长廊：https://flutter.cn/docs/development/ui/layout
 /// 代码：https://github.com/flutter/flutter/blob/master/dev/integration_tests/flutter_gallery/lib/demo/material/grid_list_demo.dart
@@ -37,6 +38,8 @@ class GirlGalleryWidget extends StatefulWidget {
 class GirlGalleryState extends State<GirlGalleryWidget> {
   late List<GirlGalleryItem> girls;
   late FavorGirl favorGirl;
+  bool isLoadingData = true;
+  int dataSource = 0; // 0，代码生成；1，数据库
 
   void _favorGirl(int index, bool isFavor) {
     setState(() {
@@ -47,32 +50,73 @@ class GirlGalleryState extends State<GirlGalleryWidget> {
   /// https://medium.flutterdevs.com/staggered-gridview-in-flutter-661f6667595d
   @override
   Widget build(BuildContext context) {
-    return StaggeredGridView.countBuilder(
-      //主轴方向
-      scrollDirection: Axis.vertical,
-      //交叉轴（横轴）单元格数量
-      crossAxisCount: 2,
-      //元素总数量
-      itemCount: girls.length,
-      // 主轴（竖轴）item之间的距离
-      mainAxisSpacing: 5.0,
-      // 交叉轴（横轴）item之间的距离
-      crossAxisSpacing: 5.0,
-      itemBuilder: (context, index) {
-        return GirlGalleryCard(girlItem: girls[index], favorGirl: favorGirl);
-      },
-      // 配置主轴和交叉轴的长度比例
-      staggeredTileBuilder: (index) {
-        // 描述主轴与交叉轴与主轴的比例。第一个参数是交叉轴，第二个参数是主轴。
-        return StaggeredTile.count(1, index.isEven ? 1.5 : 1.8);
-      },
-    );
+    return createPage();
   }
 
   @override
   void initState() {
     super.initState();
-    girls = List.generate(30, (index) => GirlGalleryItem.id(index + 1));
     favorGirl = _favorGirl;
+    isLoadingData = true;
+    loadData();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    updateData();
+  }
+
+  /// 页面关闭时更新数据库
+  void updateData() async {
+    if (dataSource == 1) {
+      for (var element in girls) {
+        await updateGirl(element);
+      }
+    }
+  }
+
+  /// 读取数据库中的数据
+  void loadData() async {
+    var data = await queryGirls();
+    if (data.isEmpty) {
+      dataSource = 0;
+      girls = List.generate(30, (index) => GirlGalleryItem.id(index + 1));
+    } else {
+      dataSource = 1;
+      girls = data;
+    }
+    setState(() {
+      isLoadingData = false;
+    });
+  }
+
+  Widget createPage() {
+    if (isLoadingData) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return StaggeredGridView.countBuilder(
+        //主轴方向
+        scrollDirection: Axis.vertical,
+        //交叉轴（横轴）单元格数量
+        crossAxisCount: 2,
+        //元素总数量
+        itemCount: girls.length,
+        // 主轴（竖轴）item之间的距离
+        mainAxisSpacing: 5.0,
+        // 交叉轴（横轴）item之间的距离
+        crossAxisSpacing: 5.0,
+        itemBuilder: (context, index) {
+          return GirlGalleryCard(girlItem: girls[index], favorGirl: favorGirl);
+        },
+        // 配置主轴和交叉轴的长度比例
+        staggeredTileBuilder: (index) {
+          // 描述主轴与交叉轴与主轴的比例。第一个参数是交叉轴，第二个参数是主轴。
+          return StaggeredTile.count(1, index.isEven ? 1.5 : 1.8);
+        },
+      );
+    }
   }
 }
